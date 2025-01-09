e3e18f0591a78a831c8125273cfb0555a650d50c
Paul Chibamba <paulchibamba@gmail.com>
2025-01-09 22:03:45 +0200

    refactor(Task): Add more properties to Task model

--- app/src/main/java/com/paulchibamba/deeply/model/Task.kt
+++ app/src/main/java/com/paulchibamba/deeply/model/Task.kt
@@ -2,6 +2,10 @@ package com.paulchibamba.deeply.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import kotlin.time.Duration
 
 @Entity(tableName = "task_table")
 data class Task(
@@ -9,5 +13,16 @@ data class Task(
     val id: Int = 0,
     val title: String,
     val description: String,
-    val priority: Priority
+    val priority: Priority,
+    val category: TaskCategory,
+    val status: TaskStatus,
+    val estimatedDuration: Duration,
+    val actualDuration: Duration? = null,
+    val scheduledDate: LocalDate,
+    val scheduledStartTime: LocalTime?,
+    val completedAt: LocalDateTime? = null,
+    val parentTaskId: Long? = null,
+    val projectId: Long? = null,
+    val isRepeating: Boolean = false,
+    val repeatPattern: RepeatPattern? = null
 )
