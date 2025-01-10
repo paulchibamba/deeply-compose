40defb921b3fc0154e83f6ac294a10a325b402a9
Paul Chibamba <paulchibamba@gmail.com>
2025-01-10 23:11:34 +0200

    feat(di): setup dagger-hilt in application

---
+++ app/src/main/java/com/paulchibamba/deeply/DeeplyApplication.kt
@@ -0,0 +1,8 @@
+package com.paulchibamba.deeply
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DeeplyApplication: Application() {
+}
---
+++ app/src/main/java/com/paulchibamba/deeply/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.paulchibamba.deeply.utils
+
+object Constants {
+    const val DATABASE_NAME = "deeply_database"
+}
