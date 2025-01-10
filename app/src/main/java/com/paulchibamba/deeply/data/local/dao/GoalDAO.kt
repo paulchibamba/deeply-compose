0eef3cc0173f8e998f262f7d1deeb29b3891f860
Paul Chibamba <paulchibamba@gmail.com>
2025-01-10 10:40:02 +0200

    feat(room): Add GoalDAO interface

---
+++ app/src/main/java/com/paulchibamba/deeply/data/local/dao/GoalDAO.kt
@@ -0,0 +1,42 @@
+package com.paulchibamba.deeply.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.paulchibamba.deeply.model.Goal
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface GoalDAO {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addGoal(goal: Goal)
+
+    @Query("SELECT * FROM Goal")
+    fun getAll(): Flow<List<Goal>>
+
+    @Query("SELECT * FROM Goal WHERE id = :id")
+    fun getGoal(id: Long): Flow<Goal>
+
+    @Query("SELECT * FROM Goal WHERE type = :type")
+    fun getGoalByType(type: String): Flow<List<Goal>>
+
+    @Query("SELECT COUNT(*) FROM Goal")
+    fun getGoalCount(): Flow<Int>
+
+    @Update
+    suspend fun updateGoal(goal: Goal)
+
+    @Delete
+    suspend fun deleteGoal(goal: Goal)
+
+    @Query("DELETE FROM Goal")
+    suspend fun deleteAllGoals()
+
+    @Query("SELECT * FROM Goal WHERE title LIKE :query OR description LIKE :query OR type LIKE :query")
+    fun searchGoals(query: String): Flow<List<Goal>>
+
+}
