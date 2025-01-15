049d4ec38267e4032f486a4b23ce74a0326472ac
Paul Chibamba <paulchibamba@gmail.com>
2025-01-15 21:17:20 +0200

    feat(TaskList): add actions to app bar
    -Search action
    -Sort action
    -More action

--- app/src/main/java/com/paulchibamba/deeply/ui/screens/planning/tasks/TaskListAppBar.kt
+++ app/src/main/java/com/paulchibamba/deeply/ui/screens/planning/tasks/TaskListAppBar.kt
@@ -1,26 +1,54 @@
 package com.paulchibamba.deeply.ui.screens.planning.tasks
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.paulchibamba.deeply.R
+import com.paulchibamba.deeply.model.Priority
+import com.paulchibamba.deeply.ui.components.PriorityItem
+import com.paulchibamba.deeply.ui.theme.MEDIUM_PADDING
 
 @Composable
 fun TaskListAppBar(){
-    DefaultTaskListAppBar()
+    DefaultTaskListAppBar(
+        onSearchClicked = {},
+        onSortClicked = {},
+        onDeleteAllCLicked = {}
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DefaultTaskListAppBar(){
+fun DefaultTaskListAppBar(
+    onSearchClicked: () -> Unit,
+    onSortClicked: (Priority) -> Unit,
+    onDeleteAllCLicked: () -> Unit
+){
     TopAppBar(
-        title = {
-            Text(text = "Tasks")
-        },
-        actions = {/*TODO*/},
+        title = { Text(text = "Tasks") },
+        actions = { TaskListAppBarActions(
+            onSearchClicked,
+            onSortClicked,
+            onDeleteAllCLicked
+        ) },
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer,
             titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
@@ -30,8 +58,121 @@ fun DefaultTaskListAppBar(){
     )
 }
 
+@Composable
+fun TaskListAppBarActions(
+    onSearchClicked: () -> Unit,
+    onSortClicked: (Priority) -> Unit,
+    onDeleteAllCLicked: () -> Unit
+){
+    SearchAction(onSearchClicked)
+    SortAction(onSortClicked)
+    MoreAction(onDeleteAllCLicked)
+}
+
+@Composable
+fun SearchAction(onSearchClicked: () -> Unit) {
+    IconButton(
+        onClick = { onSearchClicked() }
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Search,
+            contentDescription = stringResource(R.string.search_icon),
+            tint = MaterialTheme.colorScheme.onSurface
+        )
+    }
+}
+
+@Composable
+fun SortAction(
+    onSortClicked: (Priority) -> Unit
+) {
+
+    var expanded by remember { mutableStateOf(false) }
+    IconButton(
+        onClick = { expanded = true }
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_filter_list_24),
+            contentDescription = stringResource(R.string.sort_icon),
+            tint = MaterialTheme.colorScheme.onSurface
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+        ) {
+            DropdownMenuItem(
+                text = {
+                    PriorityItem(priority = Priority.LOW)
+                },
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.LOW)
+                }
+            )
+            DropdownMenuItem(
+                text = {
+                    PriorityItem(priority = Priority.HIGH)
+                },
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.HIGH)
+                }
+            )
+            DropdownMenuItem(
+                text = {
+                    PriorityItem(priority = Priority.NONE)
+                },
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.NONE)
+                }
+            )
+            
+        }
+        
+    }
+}
+
+@Composable
+fun MoreAction(
+    onDeleteAllTasksCLicked: () -> Unit
+){
+    var expanded by remember { mutableStateOf(false) }
+    IconButton(
+        onClick = { expanded = true }
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_more_vert_24),
+            contentDescription = stringResource(R.string.more_options_icon),
+            tint = MaterialTheme.colorScheme.onSurface
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+        ) {
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onDeleteAllTasksCLicked()
+                },
+                text = {
+                    Text(
+                        modifier = Modifier.padding(start = MEDIUM_PADDING),
+                        text = stringResource(R.string.delete_all_tasks),
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                }
+            )
+        }
+    }
+}
+
 @Preview
 @Composable
 fun DefaultTaskListAppBarPreview(){
-    DefaultTaskListAppBar()
+    DefaultTaskListAppBar(
+        onSearchClicked = {},
+        onSortClicked = {},
+        onDeleteAllCLicked = {}
+    )
 }
---
+++ app/src/main/java/com/paulchibamba/deeply/ui/theme/Dimensions.kt
@@ -0,0 +1,17 @@
+package com.paulchibamba.deeply.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val LARGE_PADDING = 16.dp
+val MEDIUM_LARGE_PADDING = 12.dp
+val MEDIUM_PADDING = 8.dp
+val SMALL_MEDIUM_PADDING = 6.dp
+val SMALL_PADDING = 4.dp
+
+val PRIORITY_INDICATOR_SIZE = 16.dp
+val PRIORITY_DROP_DOWN_HEIGHT = 60.dp
+
+val TOP_APP_BAR_HEIGHT = 56.dp
+val SEARCH_APP_BAR_HEIGHT = 96.dp
+
+val TASK_ITEM_ELEVATION = 2.dp
